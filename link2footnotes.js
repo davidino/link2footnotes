b834c624881881f150727d4587dbb1715f7adaf0
David Funaro <ing.davidino@gmail.com>
2011-08-03 23:46:05 +0200

    completed work
    added example.html completed script

--- link2footnotes.js
+++ link2footnotes.js
@@ -1,10 +1,18 @@
-$('div.body').append('<div id="foot"><ul></ul></div>');
-var link = $('div.body a');
-$.each(link, function(index,value){
-    $(this).attr('name',index);
-    var a = '<a href="'+ value +'">' + value.innerHTML + '</a>';
-    var ancor = '<a href="#'+ index +'">' + index + '</a>';
-    var span_a = '<span>' + a  + '</span>';
-    var span_ancor = '<span>' + ancor  + '</span>';
-    $('#foot ul').append('<li>'+ span_a + '&nbsp;&nbsp' + span_ancor  +'</li>');
+$(document).ready(function(){
+	$('body').append('<div id="foot"><ul></ul></div>');
+	var link = $('body a');
+	$.each(link, function(index,value){
+	    $(this).attr('name',index);
+	    $a_sup = $('<a>');
+	    $sup = $('<sup>');
+	    $sup.text(index);
+	    $a_sup.append($sup);
+	    $a_sup.attr('href','#'+'note_' + index);
+	    $(this).after($a_sup);
+	    var a = '<a href="'+ value +'" name="note_'+index+'">' + value.href + '</a>';
+	    var ancor = '<a href="#'+ index +'">' + index + '</a>';
+	    var span_a = '<span>' + a  + '</span>';
+	    var span_ancor = '<span>' + ancor  + '</span>';
+	    $('#foot ul').append('<li>'+ span_ancor + '&nbsp;&nbsp<i>' + span_a  +'</i></li>');
+	});	
 });
