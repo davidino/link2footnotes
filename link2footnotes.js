21f1ac9ad097f0fe4bdc9dc36dad4c9e7e67b94a
David Funaro <ing.davidino@gmail.com>
2011-08-04 18:36:28 +0200

    code refactoring
    some refactoring

--- link2footnotes.js
+++ link2footnotes.js
@@ -1,18 +1,32 @@
 $(document).ready(function(){
-	$('body').append('<div id="foot"><ul></ul></div>');
-	var link = $('body a');
-	$.each(link, function(index,value){
-	    $(this).attr('name',index);
-	    $a_sup = $('<a>');
-	    $sup = $('<sup>');
-	    $sup.text(index);
-	    $a_sup.append($sup);
-	    $a_sup.attr('href','#'+'note_' + index);
-	    $(this).after($a_sup);
-	    var a = '<a href="'+ value +'" name="note_'+index+'">' + value.href + '</a>';
-	    var ancor = '<a href="#'+ index +'">' + index + '</a>';
-	    var span_a = '<span>' + a  + '</span>';
-	    var span_ancor = '<span>' + ancor  + '</span>';
-	    $('#foot ul').append('<li>'+ span_ancor + '&nbsp;&nbsp<i>' + span_a  +'</i></li>');
-	});	
+  
+  $('body').append('<div id="foot"><ul></ul></div>');
+  
+  var link = $('body a')/*.find('selector')*/;
+  
+  $.each(link, function(index,value,number_class, link_class){
+    
+      /**######### Definition class #####*/
+      
+      var number_class = 'number';
+      var link_class   = 'link';
+      var li_class     = 'li'
+      var ul_class     = 'ul'
+      
+      /**##############################*/
+      
+      $(this).attr('name',index);
+      $a_sup = $('<a>');
+      $sup = $('<sup>').text(index);
+      $a_sup.append($sup).attr('href','#'+'note_' + index);
+      $(this).after($a_sup);
+      
+      var $a = $('<a>').attr('href', value.href).attr('name','note_' + index).text(value.href);      
+      var $ancor = $('<a>').attr('href', '#'+index).text(index);
+      
+      var $span_a = $('<span>').addClass(number_class).append($a);
+      var $span_ancor = $('<span>').addClass(link_class).append($ancor);
+      
+      $('#foot ul').addClass(ul_class).append($('<li>').addClass(li_class).append($span_ancor).append($span_a));
+  });  
 });
